d33f590cc9b468df40d72e17bfd88e368ad86987
Ivan Zarivnyy <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-11-30 19:42:17 +0100

    23443

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,7 +7,7 @@ export const getContactsController = async (req, res, next) => {
   try {
     const result = await contactAllService();
     res.status(200).json({
-      status: 'Successfully found contacts!',
+      status: 200,
       data: result,
     });
   } catch (error) {
@@ -21,10 +21,12 @@ export const getContactsByIdController = async (req, res, next) => {
     const result = await contactFindIdService(contactId);
     if (!result) {
       return res.status(404).json({
+        status: 404,
         message: `Contact with id ${contactId} not found`,
       });
     }
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: result,
     });
