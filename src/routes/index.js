c2c85d5628a1cbbd0f412a040a4ac32955545e3c
Ivan Zarivnyy <144140704+IvanZarivnyy007@users.noreply.github.com>
2025-01-20 20:36:53 +0100

    qwertyuieww

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import contactsRouter from '../routes/contacts.js';
+import authRouter from './auth.js';
 
 const mainRouter = Router();
 
 mainRouter.use('/contacts', contactsRouter);
+
+mainRouter.use('/auth', authRouter);
 
 export default mainRouter;
